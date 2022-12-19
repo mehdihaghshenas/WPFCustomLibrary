d6df338513bef02804cdfd30fb76f9260e172d12
Mehdi <Mehdi@DESKTOP-RM4UNHO>
2022-12-20 01:20:45 +0330

    change Commnets

--- MAction.WpfCustomControlLibrary/MyRichTextBox.cs
+++ MAction.WpfCustomControlLibrary/MyRichTextBox.cs
@@ -24,28 +24,29 @@ namespace MAction.WpfCustomControlLibrary
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfApp1"
+    ///     xmlns:MyNamespace="clr-namespace:MAction.WpfCustomControlLibrary"
     ///
     ///
     /// Step 1b) Using this custom control in a XAML file that exists in a different project.
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfApp1;assembly=WpfApp1"
+    ///     xmlns:MyNamespace="clr-namespace:MAction.WpfCustomControlLibrary;assembly=MAction.WpfCustomControlLibrary"
     ///
     /// You will also need to add a project reference from the project where the XAML file lives
     /// to this project and Rebuild to avoid compilation errors:
     ///
     ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///     "Add Reference"->"Projects"->[Select this project]
     ///
     ///
     /// Step 2)
     /// Go ahead and use your control in the XAML file.
     ///
-    ///     <MyNamespace:MyCustomControl/>
+    ///     <MyNamespace:CustomControl1/>
     ///
     /// </summary>
+
     public class MyRichTextBox : //RichTextBox
                                  FlowDocumentScrollViewer
     {
